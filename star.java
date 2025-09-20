1a530bfb0f4c70dcc8c60b9d31ccd3cd1cfc0d42
Tessa Tomy <tessatomy757@gmail.com>
2025-09-21 03:53:23 +0530

    init: legacy applet with polygon render and embedded viewer tag

---
+++ star.java
@@ -0,0 +1,28 @@
+/*
+ * [initial-upload] Legacy applet rendering using embedded <applet> tag
+ * [jdk-1.8] Compatible with appletviewer.exe; tested polygon rendering
+ * [graphics] White star on black background using fillPolygon
+ * [run-instructions] Compile with `javac star.java`, then run using `appletviewer star.java`
+ * [notes] Embedded tag must be JavaDoc-style 
+ */
+   
+import java.awt.*;
+import java.applet.*;
+
+public class star extends Applet
+{
+	public void init()
+	{
+		setBackground(Color.BLACK);
+	}
+	public void paint(Graphics g)
+	{
+		g.setColor(Color.WHITE);
+		int x[]={50,200,250,300,450,350,400,250,100,150};
+		int y[]={130,130,50,130,130,175,250,190,250,175};
+		g.fillPolygon(x,y,10);
+	}
+}
+
+/*<applet code="star.class" height=500 width=500>
+</applet>*/
